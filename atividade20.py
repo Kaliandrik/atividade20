cecde25f89ff61e3949087d0843c8a5887204f45
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-18 16:05:20 -0300

    Somar todos os números pares de 1 a 100.

---
+++ atividade20.py
@@ -0,0 +1,13 @@
+# Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
+
+valor = 0
+
+for n in range(1, 101):
+
+    if n %2 == 0:
+
+
+        valor = valor + n
+print(valor)
+        
+        
